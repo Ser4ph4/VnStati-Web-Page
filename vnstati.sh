7fdbe868d2d82edce0073d22d2aebe519c1d096b
Rodrigo Brito <78483475+Ser4ph4@users.noreply.github.com>
2024-07-21 14:04:24 -0300

    .SH

--- vnstati.sh
+++ vnstati.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-vnstati -s -i wg0 -o /var/www/html/vnstati/wg0_vnstat_s.png
-vnstati -5 -i wg0 -o /var/www/html/vnstati/wg0_vnstat_5.png
-vnstati -h -i wg0 -o /var/www/html/vnstati/wg0_vnstat_h.png
-vnstati -d -i wg0 -o /var/www/html/vnstati/wg0_vnstat_d.png
-vnstati -m -i wg0 -o /var/www/html/vnstati/wg0_vnstat_m.png
-vnstati -y -i wg0 -o /var/www/html/vnstati/wg0_vnstat_y.png
-vnstati -t -i wg0 -o /var/www/html/vnstati/wg0_vnstat_t.png
-vnstati -s -i ens3 -o /var/www/html/vnstati/ens3_vnstat_s.png
-vnstati -5 -i ens3 -o /var/www/html/vnstati/ens3_vnstat_5.png
-vnstati -h -i ens3 -o /var/www/html/vnstati/ens3_vnstat_h.png
-vnstati -d -i ens3 -o /var/www/html/vnstati/ens3_vnstat_d.png
-vnstati -m -i ens3 -o /var/www/html/vnstati/ens3_vnstat_m.png
-vnstati -y -i ens3 -o /var/www/html/vnstati/ens3_vnstat_y.png
-vnstati -t -i ens3 -o /var/www/html/vnstati/ens3_vnstat_t.png
+vnstati -vs -i eth0 -o /var/www/html/vnstati/sumario.png
+vnstati -h -i eth0 -o /var/www/html/vnstati/horas.png
+vnstati -d -i eth0 -o /var/www/html/vnstati/dias.png
+vnstati -m -i eth0 -o /var/www/html/vnstati/meses.png
+vnstati -y -i eth0 -o /var/www/html/vnstati/anos.png
+vnstati -t -i eth0 -o /var/www/html/vnstati/top.png
